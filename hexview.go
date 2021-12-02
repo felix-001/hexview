59adba51129306f41187b22193b954f0f859f242
rigensen <mr.right.0005@gmail.com>
2021-12-02 08:12:38 +0800

    update

--- hexview.go
+++ hexview.go
@@ -25,11 +25,10 @@ type HexView struct {
 	firstLinePos  int
 	secondLinePos int
 	asciiPos      int
-	data          []byte
 	painter       *gui.QPainter
 }
 
-func New(data []byte) *HexView {
+func New() *HexView {
 	scrollArea := *widgets.NewQAbstractScrollArea(nil)
 	scrollArea.SetFocusPolicy(core.Qt__StrongFocus)
 	ch := core.NewQChar8(core.NewQLatin1Char("9"))
@@ -44,7 +43,6 @@ func New(data []byte) *HexView {
 		QAbstractScrollArea: scrollArea,
 		charW:               charW,
 		charH:               charH,
-		data:                data,
 		painter:             nil,
 		hexPos:              hexPos,
 		firstLinePos:        firstLinePos,
@@ -69,19 +67,25 @@ func (self *HexView) addr2Text(addr int) string {
 	return fmt.Sprintf("%08X", addr)
 }
 
-func (self *HexView) Show() {
+func (self *HexView) drawBasic(event *gui.QPaintEvent) {
+	self.painter = gui.NewQPainter2(self.Viewport())
+	self.VerticalScrollBar().SetPageStep(self.Viewport().Height() / self.charH)
+	self.VerticalScrollBar().SetRangeDefault(0, 100)
+	self.painter.DrawLine3(self.firstLinePos, event.Rect().Top(), self.firstLinePos, self.Height())
+	self.painter.DrawLine3(self.secondLinePos, event.Rect().Top(), self.secondLinePos, self.Height())
+}
+
+// 只显示一个条目的数据，参考wireshark的hexview
+func (self *HexView) Show(data []byte) {
 	self.ConnectPaintEvent(func(event *gui.QPaintEvent) {
-		self.painter = gui.NewQPainter2(self.Viewport())
-		self.VerticalScrollBar().SetPageStep(self.Viewport().Height() / self.charH)
-		self.VerticalScrollBar().SetRangeDefault(0, 1000)
-		self.painter.DrawLine3(self.firstLinePos, event.Rect().Top(), self.firstLinePos, self.Height())
-		self.painter.DrawLine3(self.secondLinePos, event.Rect().Top(), self.secondLinePos, self.Height())
+		self.drawBasic(event)
 		self.painter.FillRect5(100, 60, 100, 50, gui.NewQColor2(core.Qt__lightGray))
-		for line := 0; line < 60; line++ {
+		nbLine := len(data) / BytesPerLine
+		for line := 0; line < nbLine; line++ {
 			self.drawText(AddrStartPos, line*self.charH, self.addr2Text(line*BytesPerLine))
 			for i := 0; i < BytesPerLine; i++ {
-				self.drawText(self.hexPos+i*3*self.charW, line*self.charH, fmt.Sprintf("%02X", self.data[line*BytesPerLine+i]))
-				b := self.data[line*BytesPerLine+i]
+				self.drawText(self.hexPos+i*3*self.charW, line*self.charH, fmt.Sprintf("%02X", data[line*BytesPerLine+i]))
+				b := data[line*BytesPerLine+i]
 				ascii := string(b)
 				if b < 0x20 || b > 0x7e {
 					ascii = "."
@@ -95,24 +99,24 @@ func (self *HexView) Show() {
 
 func main() {
 	testStr := `
-	QPainter performs low-level painting on widgets and other paint devices. The class can draw everything from simple lines to complex shapes like pies and chords. It can also draw aligned text and pixmaps. Normally, it draws in a "natural" coordinate system, but it can in addition do view and world transformation.
-	QPainter performs low-level painting on widgets and other paint devices. The class can draw everything from simple lines to complex shapes like pies and chords. It can also draw aligned text and pixmaps. Normally, it draws in a "natural" coordinate system, but it can in addition do view and world transformation.
-	QPainter performs low-level painting on widgets and other paint devices. The class can draw everything from simple lines to complex shapes like pies and chords. It can also draw aligned text and pixmaps. Normally, it draws in a "natural" coordinate system, but it can in addition do view and world transformation.
-	QPainter performs low-level painting on widgets and other paint devices. The class can draw everything from simple lines to complex shapes like pies and chords. It can also draw aligned text and pixmaps. Normally, it draws in a "natural" coordinate system, but it can in addition do view and world transformation.
-	QPainter performs low-level painting on widgets and other paint devices. The class can draw everything from simple lines to complex shapes like pies and chords. It can also draw aligned text and pixmaps. Normally, it draws in a "natural" coordinate system, but it can in addition do view and world transformation.
-	QPainter performs low-level painting on widgets and other paint devices. The class can draw everything from simple lines to complex shapes like pies and chords. It can also draw aligned text and pixmaps. Normally, it draws in a "natural" coordinate system, but it can in addition do view and world transformation.
-	QPainter performs low-level painting on widgets and other paint devices. The class can draw everything from simple lines to complex shapes like pies and chords. It can also draw aligned text and pixmaps. Normally, it draws in a "natural" coordinate system, but it can in addition do view and world transformation.
-	QPainter performs low-level painting on widgets and other paint devices. The class can draw everything from simple lines to complex shapes like pies and chords. It can also draw aligned text and pixmaps. Normally, it draws in a "natural" coordinate system, but it can in addition do view and world transformation.
-`
+			QPainter performs low-level painting on widgets and other paint devices. The class can draw everything from simple lines to complex shapes like pies and chords. It can also draw aligned text and pixmaps. Normally, it draws in a "natural" coordinate system, but it can in addition do view and world transformation.
+			QPainter performs low-level painting on widgets and other paint devices. The class can draw everything from simple lines to complex shapes like pies and chords. It can also draw aligned text and pixmaps. Normally, it draws in a "natural" coordinate system, but it can in addition do view and world transformation.
+			QPainter performs low-level painting on widgets and other paint devices. The class can draw everything from simple lines to complex shapes like pies and chords. It can also draw aligned text and pixmaps. Normally, it draws in a "natural" coordinate system, but it can in addition do view and world transformation.
+			QPainter performs low-level painting on widgets and other paint devices. The class can draw everything from simple lines to complex shapes like pies and chords. It can also draw aligned text and pixmaps. Normally, it draws in a "natural" coordinate system, but it can in addition do view and world transformation.
+			QPainter performs low-level painting on widgets and other paint devices. The class can draw everything from simple lines to complex shapes like pies and chords. It can also draw aligned text and pixmaps. Normally, it draws in a "natural" coordinate system, but it can in addition do view and world transformation.
+			QPainter performs low-level painting on widgets and other paint devices. The class can draw everything from simple lines to complex shapes like pies and chords. It can also draw aligned text and pixmaps. Normally, it draws in a "natural" coordinate system, but it can in addition do view and world transformation.
+			QPainter performs low-level painting on widgets and other paint devices. The class can draw everything from simple lines to complex shapes like pies and chords. It can also draw aligned text and pixmaps. Normally, it draws in a "natural" coordinate system, but it can in addition do view and world transformation.
+			QPainter performs low-level painting on widgets and other paint devices. The class can draw everything from simple lines to complex shapes like pies and chords. It can also draw aligned text and pixmaps. Normally, it draws in a "natural" coordinate system, but it can in addition do view and world transformation.
+		`
 
 	app := widgets.NewQApplication(len(os.Args), os.Args)
 
 	window := widgets.NewQMainWindow(nil, 0)
 	window.SetMinimumSize2(620, 700)
 	window.SetWindowTitle("hexview")
-	hexview := New([]byte(testStr))
+	hexview := New()
 	window.SetCentralWidget(hexview)
-	hexview.Show()
+	hexview.Show([]byte(testStr))
 	window.Show()
 	app.Exec()
 }
